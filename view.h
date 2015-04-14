f37c0379a1adab3c69de9bbda707ffcaa483e130
Jamie Pham <phamj4@wit.edu>
2015-04-13 23:03:23 -0400

    Starter Code

--- view.h
+++ view.h
@@ -2,7 +2,7 @@
 #define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-#include <SDL2/SDL_mixer.h>
+#include <SDL2/SDL_mixer.h>x	
 #include <SDL2/SDL_ttf.h>
 #include "model.h"
 #include <map>
@@ -23,7 +23,12 @@ private:
     SDL_Surface* screen;
     bool fail;
     SDL_Surface* load(char * path);
-	SDL_Surface* stand;
+	SDL_Surface* Stand;
+	SDL_Surface* StandBody;
+	SDL_Surface* StandLeftArm;
+	SDL_Surface* StandRightArm;
+	SDL_Surface* StandLeftLeg;
+	SDL_Surface* StandRightLeg;
 //    SDL_Surface* text;
 //    Mix_Music * music;
 //    Mix_Chunk * food;
