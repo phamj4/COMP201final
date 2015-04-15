6c2f07ac9b4aa212bb26dc970ccc4342b9f042fd
Alex Law <lawa1@wit.edu>
2015-04-15 09:15:31 -0400

    Working on creating spaces for word

--- model.cpp
+++ model.cpp
@@ -31,3 +31,64 @@ void Model::type(char letter) {
 	
 }
 
+bool Model::visible(){
+	while (playerWrongGuesses != wrongGuesses || playerGuesses != 27)
+	{
+
+		cin << guess;
+
+		if (guess != letter)
+		{
+
+			playerWrongGuesses++;
+			playerGuesses++;
+		}
+		else
+		{
+			playerGuesses++;
+		}
+		
+		if (playerWrongGuesses = 0) {
+			Stand.visible = true;
+		}
+		if (playerWrongGuesses = 1) {
+			Stand.visible = true
+			StandHead.visible = true;
+		}
+		if (playerWrongGuesses = 2) {
+			Stand.visible = true;
+			StandHead.visible = true;
+			StandBody.visible = true;
+		}
+		if (playerWrongGuesses = 3) {
+			Stand.visible = true;
+			StandHead.visible = true;
+			StandBody.visible = true;
+			StandLeftArm.visible = true;
+		}
+		if (playerWrongGuesses = 4) {
+			Stand.visible = true;
+			StandHead.visible = true;
+			StandBody.visible = true;
+			StandLeftArm.visible = true;
+			StandRightArm.visbile = true;
+		}
+		if (playerWrongGuesses = 5) {
+			Stand.visible = true;
+			StandHead.visible = true;
+			StandBody.visible = true;
+			StandLeftArm.visible = true;
+			StandRightArm.visible = true;
+			StandLeftLeg.visible = true;
+		}
+		if (playerWrongGuesses = 6) {
+			Stand.visible = true;
+			StandHead.visible = true;
+			StandBody.visible = true;
+			StandLeftArm.visible = true;
+			StandRightArm.visible = true;
+			StandLeftLeg.visible = true;
+		}
+	}
+	
+}
